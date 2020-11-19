38f1e872cdbeab356e218aee2f3176a313dc6954
Erinle Feyijimi <erinlejhimmy@gmail.com>
2020-11-19 11:46:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,19 +13,15 @@ document.querySelector('.check').addEventListener('click', function () {
       '⛔ Please Enter A Valid Input';
   }
 
-  if (percentage >= 30) {
+  if (percentage < 31) {
     document.querySelector(
       '.message-paragraph'
     ).textContent = `Oops! Sorry This relationship between ${firstName} and ${crushName} has no chance of working! Nature sometimes do play trick on us. Your Missing rib is out there. Keep On Searching!!!`;
-  }
-
-  if (percentage >= 50) {
+  } else if (percentage < 51) {
     document.querySelector(
       '.message-paragraph'
-    ).textContent = `The chances of a relationship working out between ${firstName} and ${crushName} is not very big, but a relationship is very well possible, if the two of you really want it to, and are prepared to make some sacrifices for it. You'll have to spend a lot of quality time together. You must be aware of the fact that this relationship might not work out at all, no matter how much time you invest in it.`;
-  }
-
-  if (percentage >= 70) {
+    ).textContent = `The chance of a relationship working out between ${firstName} and ${crushName} is not very big, but a relationship is very well possible, if the two of you really want it to, and are prepared to make some sacrifices for it. You'll have to spend a lot of quality time together. You must be aware of the fact that this relationship might not work out at all, no matter how much time you invest in it.`;
+  } else if (percentage < 71) {
     document.querySelector(
       '.message-paragraph'
     ).textContent = `To be brave is to love unconditionally without expecting anything in return. ${firstName} and ${crushName} needs to put a little effort in your relationship. Things might not be working out as you would like them to, do not hesitate to talk about it with the person involved. Spend time together, talk with each other. `;
